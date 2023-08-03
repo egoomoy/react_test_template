fda7959c8936faf7fd93ce29bf5b7abd49705191
EdwinGoo <egoomoy@gmail.com>
2023-08-03 18:13:25 +0900

    add gitignore

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/user-service": {
-        target: "http://localhost:8080",
+        target: "http://localhost:80",
       },
     },
   },
